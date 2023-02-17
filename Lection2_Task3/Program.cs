425b23602cc7ca602c4e37e55513ba626d2aadea
tatiana <tatyana.harlamowa2017@yandex.ru>
2023-02-17 16:46:48 +0400

    Третья задача

---
+++ Lection2_Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите номер дня недели (от 1 до 7)  ");
+int index = int.Parse(Console.ReadLine());
+int[]array = {1,2,3,4,5,6,7};
+int n = array.Length;
+
+if (index < 6)
+{
+    Console.WriteLine("Нет ");
+}
+else 
+{
+    Console.WriteLine("Да");
+}
