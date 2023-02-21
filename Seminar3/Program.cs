ea925e654bad8e5e0532a37808ce921e3ba450ef
tatiana <tatyana.harlamowa2017@yandex.ru>
2023-02-21 10:25:15 +0400

    Task1

---
+++ Seminar3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 1
+
+Console.WriteLine("Введите 5 значное число");
+string a = Console.ReadLine();
+
+if (a.Length < 5 || a.Length > 5)
+  Console.WriteLine("Число не пятизначное");
+else
+    if (a[0] == a[4] && a[1] == a[3])
+       Console.WriteLine("Это Полиндром");
+    else
+       Console.WriteLine("Это НЕ Полиндром");
+
