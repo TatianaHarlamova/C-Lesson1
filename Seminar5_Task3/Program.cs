2ec4d6f8cec07ff8bfe59f78ab24d154e7c49489
tatiana <tatyana.harlamowa2017@yandex.ru>
2023-03-01 19:09:38 +0400

    Task3

---
+++ Seminar5_Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] Array = GetRandomArray(4, 1, 100);
+Console.WriteLine(string.Join(" , ", Array));
+Console.WriteLine($"разница между максимальным элементом и минимальным  {GetSum(Array)} ");
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+  int[] result = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    result[i] = new Random().Next(minValue, maxValue + 1);
+  }
+  return result;
+}
+
+
+
+int GetSum(int[] Array)
+{
+  int max = Array[0];
+  int min = Array[1];
+  int res = 0;
+  
+  foreach(int el in Array)
+  {
+    if(el>max)
+     max=el;
+    else
+     if(el<min)
+     min=el;
+  }
+  res=max-min;  
+  return res;
+}
+
