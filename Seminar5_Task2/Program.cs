cd9b9079655827fd8fdd1d6fea5b05279f099f2e
tatiana <tatyana.harlamowa2017@yandex.ru>
2023-03-01 19:09:28 +0400

    Task2

---
+++ Seminar5_Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] Array = GetRandomArray(4, 1, 100);
+Console.WriteLine(string.Join(" , ", Array));
+Console.WriteLine($"сумма нечетных элементов {GetSum(Array)}");
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+  int[] result = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    result[i] = new Random().Next(minValue, maxValue + 1);
+  }
+  return result;
+}
+
+
+int GetSum(int[] Array)
+{
+  int count = 0;
+  for(int i=0; i<4; i++)
+  {
+    if (i% 2 > 0)
+     count = count + Array[i];
+    else
+     count = count + 0;
+  }
+  return count;
+}
